9a82c316cd4206fdc59eaa8e5d308c72b9764434
VondakCompany <vondakcompany@gmail.com>
2025-12-13 11:36:39 +0900

    Update index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to SlideForm!</h1>
+      <p>Use your desktop builder to publish forms.</p>
+    </div>
+  );
+}
